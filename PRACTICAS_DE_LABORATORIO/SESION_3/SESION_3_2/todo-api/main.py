21866e39de0d66c99d96b6807eef1480ebadba88
Jefferson <uo297420@uniovi.es>
2026-03-27 11:13:24 +0000

    Hola mundo con FastAPI funcionando

--- PRACTICAS_DE_LABORATORIO/SESION_3/SESION_3_2/todo-api/main.py
+++ PRACTICAS_DE_LABORATORIO/SESION_3/SESION_3_2/todo-api/main.py
@@ -1,6 +1,7 @@
-def main():
-    print("Hello from todo-api!")
+from fastapi import FastAPI
 
+app = FastAPI()
 
-if __name__ == "__main__":
-    main()
+@app.get("/")
+def read_root():
+    return {"mensaje": "¡Hola mundo!"}
