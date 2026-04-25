2791c0643076f636a6c3a9f4b067fcc0b07dfacb
Jefferson <uo297420@uniovi.es>
2026-04-25 10:35:37 +0000

    Añadir script de prueba para Docker

--- PRACTICAS_DE_LABORATORIO/SESION_4/SESION_4_1/prueba.py
+++ PRACTICAS_DE_LABORATORIO/SESION_4/SESION_4_1/prueba.py
@@ -1,3 +1,15 @@
+"""
+Codigo de prueba para verificar la ejecucion de Python dentro de un contenedor Docker.
+"""
+
+# 1. Librerias
 import sys
-print("Hola mundo!")
-print(f"Soy la versión {sys.version} de Python")
+
+# 2. Funcion principal
+def main():
+    print("Hola mundo!")
+    print(f"Soy la versión {sys.version} de Python")
+
+# 3. Punto de entrada del programa
+if __name__ == "__main__":
+    main()
